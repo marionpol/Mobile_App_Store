4a30b827157dab0e2d6cf34759f390366b60c784
Marion Poljakov <147153357+marionpol@users.noreply.github.com>
2024-10-07 13:15:52 +0300

    Google login + sign in screen (#5)
    * Google login
    
    * Sign in and google fixes

--- app/(auth)/singin/signin.js
+++ app/(auth)/singin/signin.js
@@ -12,18 +12,16 @@ const signup = () => {
   const [checked, setChecked] = useState(false)
   return (
     <View style={styles.container}>
-      <AuthHeader title="Sign Up"/>
-      <Input label="Name" placeholder="John Doe"/>
+      <AuthHeader title="Sign In"/>
       <Input label="Email" placeholder="example@gmail.com"/>
       <Input isPassword label="Password" placeholder="*****"/>
 
       <View style={styles.agreeRow}>
         <Checkbox checked={checked} onCheck={setChecked} />
-        <Text style={styles.agreeText}>I agree with <Text style={styles.agreeTextBold}>Terms </Text> & <Text style={styles.agreeTextBold}>Privacy</Text></Text>
       </View>
       <Button style={styles.button} title="Sign Up" />
       <Separator text="Or sign in with"/>
-      <Text style={styles.footerText}>Already have an account? <Text style={styles.footerLink}>Sign In</Text></Text>
+      <Text style={styles.footerText}>Don't have an account? <Text style={styles.footerLink}>Sign Up</Text></Text>
     </View>
  
 )
---
+++ components/GoogleLogin/index.js
@@ -0,0 +1,14 @@
+import {Image, TouchableOpacity } from 'react-native'
+import React from 'react'
+
+import styles from './styles'
+
+const GoogleLogin = () => {
+  return (
+    <TouchableOpacity activeOpacity={0.6} style={styles.container}>
+        <Image style={styles.image} source={require('../../assets/images/Google.png')} />
+    </TouchableOpacity>
+  )
+}
+
+export default React.memo(GoogleLogin)
