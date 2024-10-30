06a2eccf3e07e4104ccea7bbfbf8d74d91411203
Marion Poljakov <marion.poljakov@gmail.com>
2024-10-30 11:40:12 +0200

    Create profile screen

--- app/CreateListing/styles.js
+++ app/CreateListing/styles.js
@@ -11,5 +11,57 @@ export const styles = StyleSheet.create({
         fontSize: 14,
         color: Colors.blue,
         marginBottom: 16
+    },
+    image: {
+        width: 100,
+        height: 100,
+        borderRadius: 8,
+        marginRight: 8,
+        marginTop: 8
+    },
+    uploadContainer: {
+        width: 100,
+        height: 100,
+        borderRadius: 8,
+        borderWidth: 1,
+        borderColor: Colors.grey,
+        borderStyle: 'dotted',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginRight: 8
+    },
+    uploadCircle: {
+        width: 32,
+        height: 32,
+         borderRadius: 20,
+         backgroundColor: Colors.lightGrey,
+         flexDirection: 'column',
+         justifyContent: 'center',
+         alignItems: 'center'
+    },
+    uploadPlus: {
+        color: Colors.white,
+        fontSize: 28,
+        marginTop: -6
+    },
+    imageRow: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        flexWrap: 'wrap'
+    },
+    imageContainer: {
+        flexDirection: 'row',
+        marginRight: 8,
+    },
+    delete: {
+        width: 24,
+        height: 24,
+        marginTop: -10,
+        marginLeft: -20
+    },
+    textarea: {
+        minHeight: 120,
+        
     }
 })
--- components/Input/styles.js
+++ components/Input/styles.js
@@ -4,7 +4,7 @@ import { Colors } from "@/constants/colors";
 
 export const styles = StyleSheet.create({
     container: {
-        marginBottom: 20
+        marginBottom: 20,
     },
     inputContainer: {
         borderWidth: 1,
@@ -16,7 +16,8 @@ export const styles = StyleSheet.create({
     input: {
         paddingHorizontal: 16,
         paddingVertical: 20,
-        flex: 1
+        flex: 1,
+
     },
     label: {
         marginBottom: 8,
@@ -27,5 +28,42 @@ export const styles = StyleSheet.create({
         width: 24,
         height: 24,
         marginHorizontal: 16
+    },
+    arrow: {
+        width: 24,
+        height: 24,
+        marginHorizontal: 16,
+        transform: [{rotate: '90deg'}]
+    },
+    placeholder: {
+        paddingHorizontal: 16,
+        paddingVertical: 20,
+        flex: 1,
+        color: Colors.lightGrey
+    },
+    modalWrapper: {
+        backgroundColor: 'rgba(0, 0, 0, 0.2)',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flex: 1
+    },
+    modalContent: {
+        backgroundColor: Colors.white,
+        borderRadius: 8,
+        padding: 16
+    },
+    optionTitle: {
+        color: Colors.black,
+        marginBottom: 16,
+        fontSize: 16
+    },
+    optionText: {
+        color: Colors.black,
+        paddingVertical: 4,
+        fontSize: 15
+    },
+    selectedOption : {
+        color: Colors.blue,
+        fontWeight: 'bold'
     }
 })
